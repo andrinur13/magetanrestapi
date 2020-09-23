99ba906f13eb041749bd13abb9752715db6474d4
= <=>
2020-09-23 22:02:40 +0700

    update routes

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -31,6 +31,9 @@ $routes->setAutoRoute(true);
 // We get a performance increase by specifying the default
 // route since we don't have to scan directories.
 $routes->get('/', 'Home::index');
+
+
+// data siswa
 $routes->get('/siswaid', 'Data_Siswa_Controller::siswaid');
 $routes->get('/siswa', 'Data_Siswa_Controller::index');
 $routes->post('/siswa', 'Data_Siswa_Controller::create');
