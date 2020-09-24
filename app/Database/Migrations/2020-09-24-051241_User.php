72b54fd7c7795ff859eae6ba5cd3170e6bcde395
= <=>
2020-09-24 12:24:50 +0700

    add migration user

---
+++ app/Database/Migrations/2020-09-24-051241_User.php
@@ -0,0 +1,58 @@
+<?php namespace App\Database\Migrations;
+
+use CodeIgniter\Database\Migration;
+
+class User extends Migration
+{
+	public function up()
+	{
+		//
+		$this->forge->addField([
+			'id_user' => [
+				'type' => 'INT',
+				'constraint' => 11,
+				'unsigned' => true,
+				'auto_increment' => true
+			],
+			'id_user_type' => [
+				'type' => 'INT',
+				'constraint' => 11
+			],
+			'name' => [
+				'type' => 'VARCHAR',
+				'constraint' => '128',
+			],
+			'email' => [
+				'type' => 'VARCHAR',
+				'constraint' => '128',
+			],
+			'password' => [
+				'type' => 'VARCHAR',
+				'constraint' => '256',
+			],
+			'created_at' => [
+				'type' => 'DATETIME',
+				'null' => TRUE
+			],
+			'updated_at' => [
+				'type' => 'DATETIME',
+				'null' => TRUE
+			],
+			'deleted_at' => [
+				'type' => 'DATETIME',
+				'null' => TRUE
+			]
+		]);
+
+		$this->forge->addKey('id_user', true);
+		$this->forge->createTable('user');
+	}
+
+	//--------------------------------------------------------------------
+
+	public function down()
+	{
+		//
+		$this->forge->dropTable('user');
+	}
+}
